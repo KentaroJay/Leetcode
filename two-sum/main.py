0cd37a0183e1bb5ef0c4bc1b5b4bffed8002189e
KentaroJay <kentaro.jay.takahashi@gmail.com>
2025-07-11 00:14:03 +0200

    cosmetic fix

--- two-sum/main.py
+++ two-sum/main.py
@@ -4,11 +4,11 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         value_index_map: dict[int, int] = dict()
-        for index, item in enumerate(nums):
-            desired_value = target - item
+        for current_index, current_item in enumerate(nums):
+            desired_value = target - current_item
             mapped_index = value_index_map.get(desired_value)
             if mapped_index is None:
-                value_index_map[item] = index
+                value_index_map[current_item] = current_index
                 continue
-            return [index, mapped_index]
+            return [current_index, mapped_index]
 
