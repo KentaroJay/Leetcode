49cc59bacdde96bc3b77e55f0ba416d371ffc6f6
KentaroJay <kentaro.jay.takahashi@gmail.com>
2025-07-11 16:12:23 +0200

    Refactored - clearer variable naming

--- two-sum/main.py
+++ two-sum/main.py
@@ -3,12 +3,12 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        value_index_map: dict[int, int] = dict()
+        value_to_index: dict[int, int] = dict()
         for index, num in enumerate(nums):
             desired_value = target - num
-            mapped_index = value_index_map.get(desired_value)
+            mapped_index = value_to_index.get(desired_value)
             if mapped_index is None:
-                value_index_map[num] = index
+                value_to_index[num] = index
                 continue
             return [index, mapped_index]
 
