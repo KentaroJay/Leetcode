2f5821ed94326fc800f2feece3980615af0536ff
KentaroJay <kentaro.jay.takahashi@gmail.com>
2025-07-11 16:11:03 +0200

    Refactored meaningless notation

--- two-sum/main.py
+++ two-sum/main.py
@@ -4,11 +4,11 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         value_index_map: dict[int, int] = dict()
-        for current_index, current_item in enumerate(nums):
-            desired_value = target - current_item
+        for index, num in enumerate(nums):
+            desired_value = target - num
             mapped_index = value_index_map.get(desired_value)
             if mapped_index is None:
-                value_index_map[current_item] = current_index
+                value_index_map[num] = index
                 continue
-            return [current_index, mapped_index]
+            return [index, mapped_index]
 
